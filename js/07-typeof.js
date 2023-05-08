f44846ec280f274069d6c004651570c1efa88cb4
Monika <412.kri@gmail.com>
2023-05-08 19:10:22 +0300

    typeof

---
+++ js/07-typeof.js
@@ -0,0 +1,26 @@
+console.clear();
+
+
+/*
+TYPEOF - operatorius duomens tipo nustatymui
+*/
+
+const a = typeof 5;   //number
+console.log(a);
+
+const b = typeof '';  //string
+console.log(b);
+
+const c = typeof true;  //boolean
+console.log(c);
+
+
+function sum(a, b) {
+    return a + b;
+} 
+const d = typeof sum;  //function
+console.log(d);
+
+const e = typeof [];  //object =  array?
+console.log(e);
+
