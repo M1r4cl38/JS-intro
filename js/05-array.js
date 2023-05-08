3c9591e773279455de61ac2bd7a8c2cba8f4107a
Monika <412.kri@gmail.com>
2023-05-08 18:35:00 +0300

    array

--- js/05-array.js
+++ js/05-array.js
@@ -21,3 +21,33 @@ console.log(vardai[0]);
 console.log(vardai[1]);
 console.log(vardai[2]);
 console.log(vardai[3]);
+
+console.clear();
+
+const marks1 = [10, 9, 8];
+const marks2 = [6, 7, 8];
+const marks3 = [5, 5, 5];
+
+function average(marks) {
+    let sum = 0;
+    sum += marks[0];
+    sum += marks[1];
+    sum += marks[2];
+    return sum / marks.length;   //marks.length - masyvo nariu skaicius. tas pats kaip     return sum /3;
+}
+
+console.log('Vidurkis 1:', marks1, '=', average(marks1));
+console.log('Vidurkis 2:', marks2, '=', average(marks2));
+console.log('Vidurkis 3:', marks3, '=', average(marks3));
+
+
+const text = 'Labas rytas';
+console.log(text);
+console.log(text[1]);
+console.log(text[2]);
+console.log(text[3]);
+console.log(text[4]);
+console.log(text[5]);
+
+console.log(text.length);  //simboliu kiekis
+
