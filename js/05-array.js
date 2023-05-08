139f82dbd13c0a722d4b7b2f6501f704f4cfb48a
Monika <412.kri@gmail.com>
2023-05-08 17:37:49 +0300

    Array

---
+++ js/05-array.js
@@ -0,0 +1,23 @@
+console.clear();
+
+/*
+ARRAY - arejus, sarasas, listas, matrica, masyvas
+*/
+
+//kintamuosius vadinti daugiskaita
+const empty = [];
+console.log(empty);
+
+                //0   1  2  3  4 - kelintas skaicius. Pradedama skaiciuoti nuo 0
+const pazymiai = [10, 2, 8, 4, 6]; //kurti tik su to paties tipo informacija
+console.log(pazymiai);
+
+//                 0          1        2       3
+const vardai = ['Petras', 'Maryte', 'Jonas', 'Ona'];
+console.log(vardai);
+
+//is masyvo galima istraukti tik viena reiksme, kuri mus domina (vienoje eiluteje)
+console.log(vardai[0]);
+console.log(vardai[1]);
+console.log(vardai[2]);
+console.log(vardai[3]);
