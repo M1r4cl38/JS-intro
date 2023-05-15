2e50d82f6a587ce09a579a295afef523c349d874
Monika <412.kri@gmail.com>
2023-05-15 19:52:49 +0300

    empty func

---
+++ uzd/1-empty.js
@@ -0,0 +1,12 @@
+console.clear();
+
+console.clear();
+
+function tusciaFunkcija() {
+    return false;
+}
+
+console.log(tusciaFunkcija());
+console.log(tusciaFunkcija(6));
+console.log(tusciaFunkcija('asdfedas'));
+console.log(tusciaFunkcija(true));
